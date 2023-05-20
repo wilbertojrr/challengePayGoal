555e782fc8709f38f88734d9dddd0a32e03801af
Wilberto Ramirez <wilbertojrr@gmail.com>
2023-05-20 12:01:34 -0300

    Initial commit

---
+++ src/test/java/com/challenge/paygoal/PayGoalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.paygoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayGoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
